313032f535d1677a9a4a00c0790b91733ab224b4
JordyGar <jordygarcias.lasmercedes@gmail.com>
2025-07-08 00:54:53 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+ // Animación de aparición
+const buttons = document.querySelectorAll('.locations button');
+
+buttons.forEach((btn, index) => {
+  setTimeout(() => {
+    btn.classList.add('animate');
+  }, 200 * index);
+});
+
+// Rebote y redirección
+buttons.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    btn.classList.add('clicked');
+    
+    setTimeout(() => {
+      btn.classList.remove('clicked');
+      const link = btn.getAttribute('data-link');
+      if (link) {
+        window.location.href = link;
+      }
+    }, 300); // Espera a que termine la animación
+  });
+});
